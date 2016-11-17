6f3a33e6600316b9cd6d48b4f45f73dcb008588c
Ion Gorincioi <gorincioii@yahoo.com>
2016-11-17 14:09:53 -0800

    Complete solution for the challenge

---
+++ mergeArrays.js
@@ -0,0 +1,27 @@
+function mergeArrays(arr1, arr2){
+  let output = [];
+  let i = 0;
+  let j = 0;
+ 
+  while( i < arr1.length && j < arr2.length){
+    if (arr1[i] <= arr2[j]){
+      output.push(arr1[i]);
+        i++;            
+    } else {
+      output.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while(i < arr1.length){
+    output.push(arr1[i]);
+    i++;
+  }
+  
+  while(j < arr2.length){
+    output.push(arr1[j]);
+    j++;
+  }
+}
+
+// Big O = O(n)
